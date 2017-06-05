3b86e671c4e59c397469ef77ae1ca9c300fbcba4
zeee1 <miw5281@gmail.com>
2017-06-05 21:53:17 +0900

    경로그리기

--- R/getMostUsedPath.R
+++ R/getMostUsedPath.R
@@ -39,29 +39,16 @@ getMostUsedPath <- function(){
     resultDF[resultDF$RETURN_STATION == returnStat,]$Return_lat <- tashuStationData[tashuStationData$KIOSKNUM == returnStat,]$GEODATA_lat
   }
 
-  rentStationGeoDF <- data.frame(lon = resultDF$Rent_lon, lat = resultDF$Rent_lat)
-  returnStationGeoDF <- data.frame(lon = resultDF$Return_lon, lat = resultDF$Return_lat)
-  markerList <- rbind(rentStationGeoDF, returnStationGeoDF)
-  pathList <- data.frame(lon,lat,group)
+  rentStationGeoDF <- data.frame(name = resultDF$RENT_STATION,lon = resultDF$Rent_lon, lat = resultDF$Rent_lat)
+  #returnStationGeoDF <- data.frame(lon = resultDF$Return_lon, lat = resultDF$Return_lat)
+  #markerList <- rbind(rentStationGeoDF, returnStationGeoDF)
+  #pathList <- data.frame(lon,lat,group)
 
-  map <- get_googlemap("daejeon", zoom = 13,
-                       markers = data.frame(lon = markerList$lon, lat = markerList$lat),
+  map <- get_googlemap(center = c(lon = 127.361197,lat = 36.358494), zoom = 13,
+                       markers = data.frame(lon = rentStationGeoDF$lon, lat = rentStationGeoDF$lat),
                        maptype = "roadmap")
 
   ggmap(map, extent = "device")+
-    geom_point(data = resultDF, aes(x = Rent_lon, y = Rent_lat))+
-    geom_text(data = resultDF, aes(label = RENT_STATION))
+    geom_text(data = rentStationGeoDF, aes(label = name))
 
-  locationInfo <- data.frame(
-    Name = c("강남", "양재", "양재시민의숲", "청계산입구", "판교", "정자"), 
-    lon = c(127.028046, 127.035140, 127.038451, 127.054769, 127.111172, 127.108367), 
-    lat = c(37.497001, 37.483368, 37.469655, 37.448196, 37.394786, 37.366777)
-  )
-  
-  p3 <- ggmap(get_googlemap("gwacheon", zoom = 11,maptype = "roadmap"))
-  p3 <- p3 + geom_point(data = locationInfo, aes(x = lon, y = lat)) +
-    geom_text(data = locationInfo, aes(label = Name), size = 4, hjust = 1.2, fontface = "bold")
-  p3 + geom_path(data = locationInfo, aes(x = lon, y = lat), color = "blue", alpha = .5, lwd = 1)
-
-  
 }
